6e82e282ac0058b3580c7d623360117c4d742a56
Dmytro Prokopenko <portal200385@gmail.com>
2025-12-01 21:56:32 +0100

    feat(shared): add DTOs and abstract repository interfaces

---
+++ src/Leaderboard.Shared/DTOs/GameDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Leaderboard.Shared.DTOs;
+
+[Serializable]
+public class GameDTO
+{
+    public List<MatchDTO> Matches { get; set; } = new();
+}
---
+++ src/Leaderboard.Shared/DTOs/PlayerDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Leaderboard.Shared.DTOs;
+
+[Serializable]
+public class PlayerDTO
+{
+    public string? Name { get; set; }
+    public int Score { get; set; }
+}
